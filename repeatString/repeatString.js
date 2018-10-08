b10e719495a0e22b315d2480ddea58aabcba016f
Raul <raul.hernandez@testeandosoftware.com>
2018-10-08 22:00:29 +0200

    Primeros ejercicios

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
-}
+const repeatString = function(stringToRepeat, numberOfTimes) {
+    let finalString = '';
+    if(numberOfTimes >= 0){
+        for (i=1; i<=numberOfTimes; i++){
+            finalString = finalString + stringToRepeat;
+        };
+        return finalString;
+    } else {
+        return('ERROR');
+    };
+};
 
 module.exports = repeatString
--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let splitedString = stringToReverse.split("");
+    let reverseArray = splitedString.reverse();
+    let joinArray = reverseArray.join("");
+    return joinArray;
 }
 
 module.exports = reverseString
